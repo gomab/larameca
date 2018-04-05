fd74a08bb662fb3e1a600d54dca853de9e7922e4
gomab <egoiomab@gamil.com>
2018-04-05 15:13:32 +0200

    Front

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,12 @@
 |
 */
 
-Route::get('/', function () {
+/*
+ * Route::get('/', function () {
     return view('welcome');
+});
+ */
+
+Route::get('/', function(){
+    return view('layouts.index');
 });
